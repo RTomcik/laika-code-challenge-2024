02f0f0818dda03c04193f250d3f2b0bbb5d40af2
RTomcik <robert.tomcik430@gmail.com>
2024-10-13 17:02:57 -0700

    Refactored the query field function, broke out a portion of the parsing logic into a recursive function to handle nested schema filters

--- sequence_table_view.py
+++ sequence_table_view.py
@@ -34,8 +34,8 @@ def _build_table_data(sg, sequences: List[dict]) -> List[dict]:
         row_data = {}
         row_data["ID"] = sequence.get("id")
         row_data["Code"] = sequence.get("code")
-        row_data["Average Cut Duration"] = _evaluate_shotgrid_query_field(sg, sequence, "sg_cut_duration")
-        # row_data["IP Versions"] = _evaluate_shotgrid_query_field(sg, sequence, "sg_ip_versions")
+        # row_data["Average Cut Duration"] = _evaluate_shotgrid_query_field(sg, sequence, "sg_cut_duration")
+        row_data["IP Versions"] = _evaluate_shotgrid_query_field(sg, sequence, "sg_ip_versions")
         table_data.append(row_data)
     table_data.sort(key=lambda x: x.get("id"))
     pprint(table_data)
@@ -71,29 +71,47 @@ def _evaluate_shotgrid_query_field(sg: shotgun_api3.Shotgun, entity: dict, query
     field_properties = field_schema[query_field]["properties"]
     query_data = field_properties["query"]["value"]
     entity_type = query_data["entity_type"]
-    filter_schemas = query_data["filters"]["conditions"]
-    summary_filters = []
-    for filter_schema in filter_schemas:
-        if filter_schema["active"] == "true":
-            # Parse out if a filter is set to "Current X"
-            filter_values = filter_schema["values"]
-            replaced_filter_value = []
-            for filter_value in filter_values:
-                if filter_value.get("id") == 0 and filter_value.get("name") == f"Current {entity.get('type')}":
-                    replaced_filter_value.append(entity)
-                else:
-                    replaced_filter_value.append(filter_value)
-            if len(filter_values) == 1 and filter_schema["relation"] == "is":
-                replaced_filter_value = replaced_filter_value[0]
-                    
-            
-            summary_filters.append([filter_schema["path"], filter_schema["relation"], replaced_filter_value])
+    filter_schemas = query_data["filters"]
+    summary_filters = [_parse_filters_from_conditions(filter_schemas, entity)]
 
-
-    summary_fields = []
-    summary_fields.append({"field": field_properties["summary_field"]["value"], "type": field_properties["summary_default"]["value"]})
+    summary_fields = [{"field": field_properties["summary_field"]["value"], "type": field_properties["summary_default"]["value"]}]
+    # pprint(f'sg.summarize("{entity_type}", filters={summary_filters}, summary_fields={[summary_fields]})')
     summary = sg.summarize(entity_type, filters=summary_filters, summary_fields=summary_fields)
     return summary["summaries"][query_field]
+
+
+def _parse_filters_from_conditions(conditions_schema, entity):
+    # If the filter is a dictionary then we need to parse what is inside of it
+    if type(conditions_schema) == dict:
+        # If there is a valid value, then there is filter data to be parsed
+        if "values" in conditions_schema.keys():
+            if conditions_schema.get("active") == "true":
+                filter_values = conditions_schema["values"]
+
+                # Parse if a filter is set to "Current X", substitute current entity if so
+                replaced_filter_value = []
+                for filter_value in filter_values:        
+                    if type(filter_value) is dict and filter_value.get("id") == 0 and "Current " in filter_value.get("name"):
+                        replaced_filter_value.append(entity)
+                    else:
+                        replaced_filter_value.append(filter_value)
+
+                # SG Schema seems to include a list of filters, even for a relation value of "is"
+                # If there is only one value in the list, change the filter to just be the dictionary
+                if len(filter_values) == 1 and conditions_schema["relation"] == "is":
+                    replaced_filter_value = replaced_filter_value[0]
+                    
+                return [conditions_schema["path"], conditions_schema["relation"], replaced_filter_value]
+        else:
+            # Value then this is a nested schema, run this function again on its conditions
+            return {"filters": _parse_filters_from_conditions(conditions_schema.get("conditions"), entity), "filter_operator":conditions_schema.get("logical_operator") }
+    
+    # If the filter is a list, then we have to parse each element of the list and return it
+    elif type(conditions_schema) == list:
+        filters_list = []
+        for condition in conditions_schema:
+            filters_list.append(_parse_filters_from_conditions(condition, entity))
+        return filters_list
 
 if __name__ == "__main__":
     project_id = 85
